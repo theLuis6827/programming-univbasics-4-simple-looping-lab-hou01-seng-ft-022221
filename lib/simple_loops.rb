ed91f691aff3bb3a9c7396ad89ab6e902abfc21f
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 23:19:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,12 @@ def loop_message_five_times(message)
     count += 1
   end
 end
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
